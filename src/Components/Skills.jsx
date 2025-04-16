4a1591120b46a62d97d8bc99af867462c9e5ff99
Touseef109 <154261278+Touseef4705@users.noreply.github.com>
2025-04-17 00:46:59 +0500

    Update Skills.jsx

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,44 +1,38 @@
 import "../App.css"
 import "../Css Component/skills.css"
-import { FaHtml5,FaCss3 , FaBootstrap  } from "react-icons/fa";
+import { FaHtml5, FaCss3, FaBootstrap, FaReact } from "react-icons/fa";
+import { TbBrandRedux } from "react-icons/tb";
+import { RiFirebaseFill, RiTailwindCssFill } from "react-icons/ri";
+import { BiLogoTypescript } from "react-icons/bi";
+import { IoLogoJavascript } from "react-icons/io5";
+import { RiNextjsFill } from "react-icons/ri";
+
 
 function Skills() {
+
+    const skills = [
+        { name: "HTML5", icon: <FaHtml5 className="h-16 w-16 " /> },
+        { name: "CSS3", icon: <FaCss3 className="h-16 w-16 " /> },
+        { name: "Bootstrap", icon: <FaBootstrap className="h-16 w-16 " /> },
+        { name: "Tailwind", icon: <RiTailwindCssFill className="h-16 w-16 " /> },
+        { name: "JavaScript", icon: <IoLogoJavascript className="h-16 w-16 " /> },
+        { name: "TypeScript", icon: <BiLogoTypescript className="h-16 w-16 " /> },
+        { name: "Firebase", icon: <RiFirebaseFill className="h-16 w-16 " /> },
+        { name: "React", icon: <FaReact className="h-16 w-16 " /> },
+        { name: "Next.js", icon: <RiNextjsFill className="h-16 w-16 " /> },
+        { name: "Redux", icon: <TbBrandRedux className="h-16 w-16 " /> },
+    ];
+
     return (
         <div className="skills">
             <h1 className="dream_font text-5xl mt-5 ml-10 underline text-center"><span className="text-green-500 underline">Ski</span>lls</h1>
             <div className="skills_box">
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <FaHtml5 className="h-16 w-16"  />
-                    <b>HTML 5</b>
-                </div>
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <FaCss3 className="h-16 w-16"  />
-                    <b>CSS3</b>
-                </div>
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <FaBootstrap className="h-16 w-16" />
-                    <b>BOOTSTRAP</b>
-                </div>
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <img src="/skills/tailwind.png" className="h-16 w-22" alt="" />
-                    <b>TAILWIND</b>
-                </div>
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <img src="/skills/js.png" className="h-16 w-22" alt="" />
-                    <b>JAVASCRIPT</b>
-                </div>
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <img src="/skills/ts.png" className="h-16 w-22" alt="" />
-                    <b>TYPESCRIPT</b>
-                </div>
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <img src="/skills/firebase.png" className="h-16 w-22" alt="" />
-                    <b>FIREBASE</b>
-                </div>
-                <div className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
-                    <img src="/skills/atom.png" className="h-16 w-22" alt="" />
-                    <b>REACT</b>
-                </div>
+                {skills.map((skill, index) => (
+                    <div key={index} className="h-32 w-40 bg-gray-100 text-black flex flex-col justify-center items-center rounded-md">
+                        {skill.icon}
+                        <b>{skill.name}</b>
+                    </div>
+                ))}
             </div>
         </div>
     )
